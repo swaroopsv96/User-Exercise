c6c84e4bb176fa17077c31d21136e390b2f8f8ce
Swaroop <swaroopsv96@gmail.com>
2022-07-18 07:36:52 +0530

    Commiting main application file

---
+++ src/main/java/com/exercise/crud/UserExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserExerciseApplication.class, args);
+	}
+
+}
